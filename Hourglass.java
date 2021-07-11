88cccebfce2d5745f315cf35a5dcb6db48761a9e
Maria <mciuculan@gmail.com>
2021-07-11 16:13:04 +0300

    Problema cu matrice

---
+++ Hourglass.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+public class Hourglass {
+    private static int hourglassSum(List<List<Integer>> arr) {
+        int sum = -65;
+        for (int i = 0; i < arr.size() - 2; i++) {
+            for (int j = 0; j < arr.get(i).size() - 2; j++) {
+                int intermediateSum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2)
+                        + arr.get(i+1).get(j+1) + arr.get(i+2).get(j) + arr.get(i+2).get(j+1)
+                        + arr.get(i+2).get(j+2);
+                if (intermediateSum > sum)
+                    sum = intermediateSum;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = hourglassSum(arr);
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
